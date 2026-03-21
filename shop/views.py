bb31947fd66f4445ef5c3b5ad5abfcf104681fed
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-03-21 22:01:42 +0000

    Create product_detail view and template

--- shop/views.py
+++ shop/views.py
@@ -12,3 +12,14 @@ def all_products(request):
     }
 
     return render(request, "shop/shop.html", context)
+
+
+def product_detail(request, product_id):
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, "shop/product_detail.html", context)
