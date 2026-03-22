7a80ff4346e9dd18d174e2ee22ff9c1f8ab3d430
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-03-22 16:49:46 +0000

    Add filtering to shop

--- shop/views.py
+++ shop/views.py
@@ -1,28 +1,38 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
 
     products = Product.objects.all()
+    categories = Category.objects.all()
     query = None
+    selected_categories = None
 
     if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query: 
-                messages.error(request,
-                               "You didn't say what you were looking for, adventurer")
+                messages.error(
+                    request,
+                    "You didn't say what you were looking for, adventurer"
+                )
                 return redirect(reverse('shop'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+            
+        if 'category' in request.GET:
+            selected_categories = request.GET.getlist('category')
+            products = products.filter(category__type__in=selected_categories)
 
     context = {
         'products': products,
-        'searcg_term': query
+        'search_term': query,
+        'categories': categories,
+        'current_categories': selected_categories,
     }
 
     return render(request, "shop/shop.html", context)
