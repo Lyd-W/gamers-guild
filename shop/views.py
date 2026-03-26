803ccf7ea30dae230c21fb6aedd8771e17f235a9
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-03-26 17:00:00 +0000

    Add sorting menu and product count to shop

--- shop/views.py
+++ shop/views.py
@@ -8,57 +8,67 @@ from django.db.models.functions import Lower
 def all_products(request):
 
     products = Product.objects.all()
+
     query = None
-    selected_categories = None
+    selected_categories = []
     sort = None
     direction = None
 
-    if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(
-                    request,
-                    "You didn't say what you were looking for, adventurer"
-                )
-                return redirect(reverse('shop'))
+    if 'q' in request.GET:
+        query = request.GET['q']
 
-            queries = Q(name__icontains=query) | Q(
-                description__icontains=query)
-            products = products.filter(queries)
+        if not query:
+            messages.error(
+                request,
+                "You didn't say what you were looking for, adventurer"
+            )
+            return redirect(reverse('shop'))
 
-        if 'category' in request.GET:
-            selected_categories = request.GET['category'].split(',')
-            categories = Category.objects.filter(type__in=selected_categories)
-            expanded_categories = []
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
+
+    if 'category' in request.GET:
+        selected_categories = request.GET['category'].split(',')
+
+        categories = Category.objects.filter(type__in=selected_categories)
+
+        expanded_categories = []
+
         for cat in categories:
             if cat.subcategories.exists():
-                expanded_categories.extend(list(cat.subcategories.values_list('type', flat=True)))
+                expanded_categories.extend(
+                    list(cat.subcategories.values_list('type', flat=True))
+                )
             else:
                 expanded_categories.append(cat.type)
 
         selected_categories = expanded_categories
-        
+
         products = products.filter(category__type__in=selected_categories)
 
+    if 'sort' in request.GET:
+        sortkey = request.GET['sort']
+        sort = sortkey
 
-        if request.GET:
-            if 'sort' in request.GET:
-                sortkey = request.GET['sort']
-                sort = sortkey
-                if sortkey == 'name':
-                    sortkey = 'lower_name'
-                    products = products.annotate(lower_name=Lower('name'))
+        if sortkey == 'name':
+            sortkey = 'lower_name'
+            products = products.annotate(lower_name=Lower('name'))
 
-                if 'direction' in request.GET:
-                    direction = request.GET['direction']
-                    if direction == 'desc':
-                        sortkey = f'-{sortkey}'
+        elif sortkey.startswith('category'):
+            products = products.annotate(
+                lower_category=Lower('category__readable_type')
+            )
+            sortkey = 'lower_category'
 
-                products = products.order_by(sortkey)
+        if 'direction' in request.GET:
+            direction = request.GET['direction']
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
+
+        products = products.order_by(sortkey)
 
     current_sorting = f'{sort}_{direction}'
-    
+
     categories = Category.objects.filter(parent__isnull=False)
 
     context = {
