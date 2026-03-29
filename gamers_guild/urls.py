85eca2de70ca0540d3a349cb97099f0387c0f6d6
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-03-29 14:02:44 +0100

    Create bagg app, urls, view and template

--- gamers_guild/urls.py
+++ gamers_guild/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('shop/', include('shop.urls')),
+    path('bag/', include('bag.urls')),
     path('', include('home.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
