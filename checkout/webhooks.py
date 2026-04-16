7367a5c9ff87ad7f0a16d79cd3a95174686ef43b
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-04-16 16:38:57 +0100

    Set up and test Webhooks

---
+++ checkout/webhooks.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+from django.conf import settings
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from checkout.webhook_handler import StripeWH_Handler
+
+import stripe
+
+
+@require_POST
+@csrf_exempt
+def webhook(request):
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WH_SECRET
+        )
+    except ValueError as e:
+        return HttpResponse(status=400)
+
+    except stripe.error.SignatureVerificationError as e:
+        return HttpResponse(status=400)
+
+    except Exception as e:
+        return HttpResponse(content=e, status=400)
+
+    print('Success!')
+    return HttpResponse(status=200)
