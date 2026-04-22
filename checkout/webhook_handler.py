79edb09b73e300f938fd715a9424a5afa3c84f0e
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-04-22 20:23:35 +0100

    Add confirmation email logic

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from shop.models import Product
@@ -13,6 +16,25 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+        
+    def _send_confirmation_email(self, order):
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            { 'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            { 'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
+
+        
+        
 
     def handle_event(self, event):
         return HttpResponse(
@@ -122,6 +144,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=(
                 f'Webhook received: {event["type"]} '
