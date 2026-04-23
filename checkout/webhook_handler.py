e9db6716b9eea579840781ec39e2d0ab33aca616
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-04-23 11:05:50 +0100

    Remove whitespaces from webhook_handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -16,25 +16,22 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
-        
+
     def _send_confirmation_email(self, order):
         cust_email = order.email
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
-            { 'order': order})
+            {'order': order})
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
-            { 'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-        
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
         )
-
-        
-        
 
     def handle_event(self, event):
         return HttpResponse(
@@ -68,7 +65,7 @@ class StripeWH_Handler:
                 profile.default_street_address1 = shipping_details.address.line1
                 profile.default_street_address2 = shipping_details.address.line2
                 profile.default_county = shipping_details.address.state
-                profile.save()          
+                profile.save()
 
         order_exists = False
         attempt = 1
