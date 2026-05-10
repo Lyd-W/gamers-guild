b3d22adaf428516f6aa530841893e44e1665e149
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-05-10 20:00:04 +0100

    Remove ratings field from products

--- shop/models.py
+++ shop/models.py
@@ -27,8 +27,6 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=4, decimal_places=1,
-                                 null=True, blank=True)
     image = models.ImageField(upload_to='products/', null=True,
                               blank=True)
     image_URL = models.URLField(null=True, blank=True)
