e88d812e7168a2f82617d88c6ca48ee4951e963d
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-05-10 10:44:51 +0100

    Update signals.py to reduce stock quantity on successful order

--- checkout/signals.py
+++ checkout/signals.py
@@ -2,11 +2,32 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from .models import OrderLineItem
+from shop.models import ProductSize
 
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
+
+    if created:
+
+        if instance.product_size:
+
+            try:
+                product_size = ProductSize.objects.get(
+                    product=instance.product,
+                    size=instance.product_size
+                )
+
+                if product_size.stock >= instance.quantity:
+                    product_size.stock -= instance.quantity
+                else:
+                    product_size.stock = 0
+
+                product_size.save()
+
+            except ProductSize.DoesNotExist:
+                pass
 
 
 @receiver(post_delete, sender=OrderLineItem)
