2a763b3114b6da2f1728f0c0662c278ed37f5f17
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-05-14 23:12:56 +0100

    Style product add and edit pages

--- shop/forms.py
+++ shop/forms.py
@@ -1,20 +1,36 @@
 from django import forms
 from .widgets import CustomClearableFileInput
 from .models import Product, Category
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit
 
 
 class ProductForm(forms.ModelForm):
 
+    image = forms.ImageField(
+        label='Image',
+        required=False,
+        widget=CustomClearableFileInput()
+    )
+
     class Meta:
         model = Product
         fields = '__all__'
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_readable_type()) for c in categories]
 
-        self.fields['category'].choices = friendly_names
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'rounded-0'
+def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+    self.helper = FormHelper()
+    self.helper.form_method = 'post'
+    
+    self.fields['category'].queryset = Category.objects.all()
+
+    self.helper.layout = Layout(
+        Field('name', css_class='form-control rounded-0'),
+        Field('description', css_class='form-control rounded-0'),
+        Field('price', css_class='form-control rounded-0'),
+        Field('category', css_class='form-select rounded-0'),
+        Field('image'),
+        Submit('submit', 'Save Product', css_class='btn btn-primary rounded-0')
+    )
