0b002468847acdaaeaba31812ce48eb20636182e
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-05-16 10:35:00 +0100

    Fix urls pattern for custom error page testing

--- home/urls.py
+++ home/urls.py
@@ -5,12 +5,12 @@ from .views import home
 urlpatterns = [
     path("", home, name="home"),
     path("search/", views.search, name="search"),
+    path("test-400/", views.trigger_400),
+    path("test-403/", views.trigger_403),
+    path("test-500/", views.trigger_500),
     path("<slug:slug>/favourite/", views.toggle_favourite,
          name="toggle_favourite"),
     path("<slug:slug>/", views.boardgame_detail, name="boardgame_detail"),
     path("<slug:slug>/delete_review/<int:review_id>/", views.delete_review, name="delete_review"),
     path("review/<int:review_id>/approve/", views.approve_review, name="approve_review"),
-    path("test-400/", views.trigger_400),
-    path("test-403/", views.trigger_403),
-    path("test-500/", views.trigger_500),
 ]
