322367b0509ca45d08e501d6c6bd181276be3f7e
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-05-16 11:12:49 +0100

    Update error page paths

--- home/views.py
+++ home/views.py
@@ -248,15 +248,15 @@ def approve_review(request, review_id):
 
 
 def custom_400(request, exception):
-    return render(request, "errors/400.html", status=400)
+    return render(request, "400.html", status=400)
 
 
 def custom_403(request, exception):
-    return render(request, "errors/403.html", status=403)
+    return render(request, "403.html", status=403)
 
 
 def custom_500(request):
-    return render(request, "errors/500.html", status=500)
+    return render(request, "500.html", status=500)
 
 
 def trigger_400(request):
