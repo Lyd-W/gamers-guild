31b4e7715f7f78a985dd42fa72519e592fef4540
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-05-21 22:17:30 +0100

    Add caching lifetime to custom_storages.py

--- custom_storages.py
+++ custom_storages.py
@@ -4,7 +4,15 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
+    default_acl = "public-read"
+    object_parameters = {
+        "CacheControl": "max-age=31536000, public, immutable",
+    }
 
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+    default_acl = "public-read"
+    object_parameters = {
+        "CacheControl": "max-age=86400, public",
+    }
