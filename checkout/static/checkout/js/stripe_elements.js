5c50e44762e6703f50d0bcde25a28093d95019e5
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2026-05-23 14:15:26 +0100

    Add aria-hidden to svg

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -45,7 +45,8 @@ card.addEventListener('change', function (event) {
                      height="16" 
                      fill="currentColor" 
                      class="bi bi-x-lg" 
-                     viewBox="0 0 16 16">
+                     viewBox="0 0 16 16"
+                     aria-hidden="true">
                     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
                 </svg>
             </span>
@@ -148,7 +149,8 @@ form.addEventListener('submit', function (ev) {
                         height="16" 
                         fill="currentColor" 
                         class="bi bi-x-lg" 
-                        viewBox="0 0 16 16">
+                        viewBox="0 0 16 16"
+                        aria-hidden="true">
                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
                     </svg>
                 </span>
